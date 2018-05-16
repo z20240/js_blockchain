fc95eca287dd10d61288f09e3d32d4e9aabc2ff1
les <z20240z@gmail.com>
2018-05-16 18:05:00 +0800

    bcn

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 var bodyParser = require('body-parser'); // 用來 parse request.post
 const app = express();
 
+let port = 3000;
+
+// access the arguments;
+process.argv.forEach((ele, indx, ary) => {
+    console.log("ele", ele);
+    port = ary[2];
+});
+
 let Block = require('./block');
 let Blockchain = require('./blockchain');
 let Transaction = require('./transaction');
@@ -13,6 +21,7 @@ app.use(express.json());
 // 初始之鏈
 let genesisBlock = new Block();
 let blockchain = new Blockchain(genesisBlock);
+let transactions = [];
 
 app.get('/', function(req, res) {
     res.send("test");
@@ -22,13 +31,8 @@ app.post('/transactions', function(req, res) {
     let {from, to, amount} = req.body;
 
     console.log(from, to, amount);
+    transactions.push(new Transaction(from, to, amount));
 
-    let transaction = new Transaction(from, to, amount);
-    let transactions = JSON.parse(JSON.stringify(blockchain.getPreviousBlock().transactions));
-
-    transactions.push(transaction);
-    let block = blockchain.getNextBlock(transactions);
-    blockchain.addBlock(block);
     res.end();
 });
 
@@ -36,7 +40,14 @@ app.get('/blockchain', function(req, res) {
     res.json(blockchain);
 });
 
+app.post('/mining', function (req, res) {
+    let block = blockchain.getNextBlock(transactions);
+    blockchain.addBlock(block);
 
-app.listen(3000, function() {
-    console.log("server started");
+    res.json(block);
+});
+
+
+app.listen(port, function() {
+    console.log("server started", "listen on port");
 });
---
+++ blockchainNode.js
@@ -0,0 +1,11 @@
+class BlockchainNode {
+
+    constructor (url) {
+        this.url = url;
+    }
+
+
+}
+
+
+module.exports = BlockchainNode;
